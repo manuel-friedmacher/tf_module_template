79ca792371c2acc134cf4eaa1d25d5e8bab062f8
Manuel Friedmacher <74181220+Friedmacher@users.noreply.github.com>
2026-09-12 12:57:03 +0200

    Create versions.tf

---
+++ versions.tf
@@ -0,0 +1,15 @@
+# The minimum required versions of Terraform and the used providers.
+terraform {
+  required_version = ">= 1.14.0"
+
+  required_providers {
+    btp = {
+      source  = "SAP/btp"
+      version = ">= 1.22"
+    }
+    cloudfoundry = {
+      source  = "cloudfoundry/cloudfoundry"
+      version = ">= 1.15"
+    }
+  }
+}
